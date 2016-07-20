a787f2deb8467037b7923c951a320e32e2e2d695
Heather Park <hthr.prk@gmail.com>
2016-07-19 23:48:08 -0700

    Create TaskInfo component

---
+++ components/TaskInfo.js
@@ -0,0 +1,19 @@
+const TaskInfo = ({
+  title,
+  status,
+  dueDate,
+  category,
+  points
+}) => {
+  return (
+    <div>
+      {title}
+      {status}
+      Due Date: {dueDate}
+      {category}
+      {points} points
+    </div>
+  );
+};
+
+export TaskInfo;
