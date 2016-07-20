d737c7d4f9f52916277823cf61f68d0b92f47d9d
Heather Park <hthr.prk@gmail.com>
2016-07-19 23:31:43 -0700

    Write TaskCard component without nested components

---
+++ components/TaskCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class TaskCard extends React.Component {
+  render() {
+    return (
+      <div className="task-container">
+        <div className="task-info">
+          Task Title
+          Due Date
+          Status
+        </div>
+        <div class="notes">
+          <input className="note-input" type="text">
+          <ul>
+            <li>Notes</li>
+          </ul>
+        </div>
+        <div className="comments">
+          <input className="note-input" type="text">
+          Comment 1
+          Comment 2
+        </div>
+      </div>
+    );
+  }
+}
