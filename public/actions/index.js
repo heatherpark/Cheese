a37cc8a9053451b8222d789598c93160f3f66c35
Heather Park <hthr.prk@gmail.com>
2016-07-24 00:00:43 -0700

    Add filterView action

--- public/actions/index.js
+++ public/actions/index.js
@@ -1,44 +1,68 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-// export const REQUEST_TASKS = 'REQUEST_TASKS'
-// export const RECEIVE_TASKS = 'RECIEVE_TASKS'
-// export const POST_TASK = 'POST_TASK'
-
-
-// export function requestTasks() {
-//   return {
-//     type: REQUEST_TASKS
-//   }
-// }
-
-// export function receiveTasks(tasks) {
-//   return {
-//     type: RECEIVE_TASKS,
-//     tasks,
-//     data: json.data.map(task => task),
-//     receivedAt: Date.now()
-//   }
-// }
-
-// export function postTask() {
-//   return {
-//     type: POST_TASK
-//   }
-// }
-
-// export function fetchTasks() {
-//   return dispatch => {
-//     console.log('fetchTasks')
-//     return axios.get('http://localhost:3000/tasks')
-//       .then(response => {
-//         console.log(response)
-//         response.data.json()})
-//       .then(json => dispatch(receiveTasks(json)))
-//       .catch(function(error) {
-//         console.log(error);
-//       });
-//   }
-// }
+export const REQUEST_TASKS = 'REQUEST_TASKS'
+export const RECEIVE_TASKS = 'RECIEVE_TASKS'
+export const POST_TASK = 'POST_TASK'
 
 
+export function requestTasks() {
+  return {
+    type: REQUEST_TASKS
+  }
+}
 
+export function receiveTasks(tasks) {
+  return {
+    type: RECEIVE_TASKS,
+    tasks,
+    data: json.data.map(task => task),
+    receivedAt: Date.now()
+  }
+}
+
+export function postTask() {
+  return {
+    type: POST_TASK
+  }
+}
+
+export function fetchTasks() {
+  return dispatch => {
+    console.log('fetchTasks')
+    return axios.get('http://localhost:3000/tasks')
+      .then(response => {
+        console.log(response)
+        response.data.json()})
+      .then(json => dispatch(receiveTasks(json)))
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export function filterView(filter) {
+  return {
+    type: 'FILTER_VIEW',
+    filter
+  }
+}
